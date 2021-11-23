ebc55f71afecad424da086748150bc200d7549c4
Samuel Galanakis <samme.galanakis6@gmail.com>
2021-11-23 23:27:51 +0100

    Working for set coordinates"

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from data_utils import ImageDataset
 from torch import nn
 import wandb
 import einops
-
+import math
 
 def main():
     run = wandb.init(project="GeomAutoencoder", entity="samme013",
@@ -40,10 +40,12 @@ def main():
 
     model = torch.hub.load('pytorch/vision:v0.10.0',
                            'resnet50', pretrained=True)
-    model.fc = nn.Sequential(nn.Linear(2048, 1024),
-                             nn.GELU(), nn.Linear(1024, config.n_vertices*5))
+ 
+    fc_out_val = 1024
+    model.fc = nn.Linear(2048,fc_out_val)
 
-    
+    decider = nn.Linear(fc_out_val+2*config.n_vertices,config.n_vertices*3)
+    decider = decider.to(device)
     if config.checkpoint_path != "":
         state_dict = torch.load(config.checkpoint_path)
 
@@ -51,10 +53,10 @@ def main():
     model = model.to(device)
 
 
-
+    parameters+= decider.parameters()
     parameters += model.parameters()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'])
+    optimizer = torch.optim.Adam(parameters, lr=config['lr'])
 
     indexes = (torch.cartesian_prod(torch.arange(0, config.resize_size), torch.arange(
         0, config.resize_size))/(config.resize_size-1)).to(device)
@@ -63,34 +65,43 @@ def main():
     first_indexer = torch.arange(
         config.batch_size).reshape((-1, 1, 1)).to(device)
 
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor = 0.5,patience = 1000)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor = 0.5,patience = 2000)
 
     sigmoid = nn.Sigmoid()
     criterion = nn.L1Loss()
+    points = torch.rand((config.batch_size,config.n_vertices,2),device=device)
     for epoch in range(config.n_epochs):
         epoch_loss = 0
 
         for index, img in enumerate(tqdm(dataloader)):
             img = img.to(device)
-
+            
             net_out = model(img)
 
-            coordinates, colors = net_out.reshape(
-                (-1, config.n_vertices, 5)).split([2, 3], dim=-1)
-            coordinates = (coordinates - coordinates.min())
-            coordinates = coordinates/coordinates.max()
-            average_pairwise_dist = torch.cdist(
-                coordinates, coordinates).min(dim=-1)[0].mean()
-            coordinates = nn.functional.relu(coordinates)
+            decider_in = torch.cat((net_out,points.reshape((config.batch_size,-1))),-1)
+            colors = decider(decider_in)
+            colors = einops.rearrange(colors,"b (v c) -> b v c",v=config.n_vertices,c=3)
+            # coordinates, colors = net_out.reshape(
+            #     (-1, config.n_vertices, 5)).split([2, 3], dim=-1)
+
+            # Coordinate normalization
+            # coordinates = (coordinates - coordinates.min())
+            # coordinates = coordinates/coordinates.max()
+            
+            # average_pairwise_dist = torch.cdist(
+            #     coordinates, coordinates).min(dim=-1)[0]
+            # average_pairwise_dist =    torch.max(average_pairwise_dist,torch.tensor(1/math.sqrt(config.n_vertices)).cuda())
+            
+            #average_pairwise_dist = average_pairwise_dist.mean()
+            #coordinates = nn.functional.relu(coordinates)
             closest = torch.linalg.norm(
-                (coordinates.unsqueeze(2) - indexes), dim=-1).argmin(axis=1)
+                (points.unsqueeze(2) - indexes), dim=-1).argmin(axis=1)
             out_img = einops.rearrange(colors[first_indexer, closest.unsqueeze(
                 -1), last_indexer], "b (i k) n -> b n i k ", i=224, k=224)
 
             l1_loss = criterion(out_img, img)
 
-            loss = 2.5*sigmoid(l1_loss) - \
-                sigmoid(average_pairwise_dist)  # 2 kinda worked
+            loss = 1 * l1_loss  # - average_pairwise_dist  # 2 kinda worked
 
             optimizer.zero_grad()
             loss.backward()
@@ -108,7 +119,7 @@ def main():
                         "Recreated": recreated_img
                     })
             wandb.log({"Loss": loss.item(), "L1 loss": l1_loss.item(
-            ), "Pairwise dist loss": average_pairwise_dist, "lr": optimizer.param_groups[0]['lr']})
+            ),"lr": optimizer.param_groups[0]['lr']})
             epoch_loss = epoch_loss * (index)/(index+1) + loss.item()/(index+1)
 
         wandb.log({"epoch_loss": epoch_loss})
