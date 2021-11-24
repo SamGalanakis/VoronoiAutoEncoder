455a84084047168ca968c23336de159e89ea0669
Samuel Galanakis <samme.galanakis6@gmail.com>
2021-11-24 15:50:57 +0100

    Somewhat working with random point placement - transformer patches version

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from typing import ForwardRef
 import torch
 import matplotlib.pyplot as plt
+from torch.nn import parameter
+from torch.utils import data
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -8,6 +11,8 @@ from torch import nn
 import wandb
 import einops
 import math
+import torch.nn.functional as F
+from torch import einsum
 
 def main():
     run = wandb.init(project="GeomAutoencoder", entity="samme013",
@@ -34,26 +39,54 @@ def main():
 
     dataset = ImageDataset("data\celeba_hq_256", transformations)
     dataloader = DataLoader(
-        dataset, batch_size=config.batch_size, num_workers=8, pin_memory=True)
+        dataset, batch_size=config.batch_size, num_workers=8, pin_memory=True,drop_last=True)
 
     parameters = []
 
     model = torch.hub.load('pytorch/vision:v0.10.0',
-                           'resnet50', pretrained=True)
- 
-    fc_out_val = 1024
-    model.fc = nn.Linear(2048,fc_out_val)
+                           'resnet18', pretrained=True)
+    
 
-    decider = nn.Linear(fc_out_val+2*config.n_vertices,config.n_vertices*3)
-    decider = decider.to(device)
+   
+
+    class VoronoiNet(nn.Module):
+        def __init__(self,patch_embed_dim=64,inner_dim=32):
+            super().__init__()
+            self.patch_embed_dim = patch_embed_dim
+            self.inner_dim = inner_dim 
+            self.conv_layers = nn.Sequential(torch.nn.Conv2d(3, 100,16,16,0,bias=False))
+
+            self.patch_mapper = nn.Linear(768,64)
+            self.coordinate_to_key= nn.Sequential(nn.Linear(2,inner_dim,bias=False),nn.ReLU(),nn.Linear(inner_dim,inner_dim,bias=False))
+            self.positional_encoding = nn.Parameter(torch.randn((1,729,768),requires_grad=True))
+            self.patch_to_key_val =  nn.Linear(patch_embed_dim, inner_dim * 2, bias=False)
+            self.color_mapper = nn.Sequential( nn.Linear(inner_dim,inner_dim),nn.GELU(),nn.Linear(inner_dim,256),nn.GELU(),nn.Linear(256,64), nn.GELU(), nn.Linear(64,3))     
+        def forward(self,x,coordinates):
+            patches = x.unfold(2, 16, 8).unfold(3, 16, 8)
+            patches = einops.rearrange(patches,"b c i j k m -> b (i j) (c k m)")
+            patches = patches + self.positional_encoding
+            patches = self.patch_mapper(patches)
+
+            q = self.coordinate_to_key(coordinates)
+            k,v = self.patch_to_key_val(patches).split((self.inner_dim,self.inner_dim),-1)
+            sim = einsum('b i d, b j d -> b i j', q, k)
+            attn = sim.softmax(dim=-1)
+            out = einsum('b i j, b j d -> b i d', attn, v)
+            colors = self.color_mapper(out)
+            return colors  
+
+
+    model = VoronoiNet().to(device)
+
     if config.checkpoint_path != "":
         state_dict = torch.load(config.checkpoint_path)
 
         model.load_state_dict(state_dict)
     model = model.to(device)
 
-
-    parameters+= decider.parameters()
+    
+    
+    
     parameters += model.parameters()
 
     optimizer = torch.optim.Adam(parameters, lr=config['lr'])
@@ -65,43 +98,32 @@ def main():
     first_indexer = torch.arange(
         config.batch_size).reshape((-1, 1, 1)).to(device)
 
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor = 0.5,patience = 2000)
-
-    sigmoid = nn.Sigmoid()
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor = 0.5,patience = len(dataloader))
+    
     criterion = nn.L1Loss()
-    points = torch.rand((config.batch_size,config.n_vertices,2),device=device)
+
+
+
+    
+    
     for epoch in range(config.n_epochs):
         epoch_loss = 0
 
         for index, img in enumerate(tqdm(dataloader)):
             img = img.to(device)
-            
-            net_out = model(img)
 
-            decider_in = torch.cat((net_out,points.reshape((config.batch_size,-1))),-1)
-            colors = decider(decider_in)
-            colors = einops.rearrange(colors,"b (v c) -> b v c",v=config.n_vertices,c=3)
-            # coordinates, colors = net_out.reshape(
-            #     (-1, config.n_vertices, 5)).split([2, 3], dim=-1)
+            coordinates = torch.rand((config.batch_size,config.n_vertices,2),device=device)
+            colors = model(img,coordinates)
 
-            # Coordinate normalization
-            # coordinates = (coordinates - coordinates.min())
-            # coordinates = coordinates/coordinates.max()
             
-            # average_pairwise_dist = torch.cdist(
-            #     coordinates, coordinates).min(dim=-1)[0]
-            # average_pairwise_dist =    torch.max(average_pairwise_dist,torch.tensor(1/math.sqrt(config.n_vertices)).cuda())
-            
-            #average_pairwise_dist = average_pairwise_dist.mean()
-            #coordinates = nn.functional.relu(coordinates)
             closest = torch.linalg.norm(
-                (points.unsqueeze(2) - indexes), dim=-1).argmin(axis=1)
+                (coordinates.unsqueeze(2) - indexes), dim=-1).argmin(axis=1)
             out_img = einops.rearrange(colors[first_indexer, closest.unsqueeze(
-                -1), last_indexer], "b (i k) n -> b n i k ", i=224, k=224)
+                -1), last_indexer], "b (i k) n -> b n i k ", i=config.resize_size, k=config.resize_size)
 
             l1_loss = criterion(out_img, img)
 
-            loss = 1 * l1_loss  # - average_pairwise_dist  # 2 kinda worked
+            loss =  l1_loss  
 
             optimizer.zero_grad()
             loss.backward()
